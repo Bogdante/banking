1ea407a6cb266d68851002f2a84e0bdfc9dddf8c
Bogdante <bogdantegiero@gmail.com>
2024-12-09 22:57:15 +0300

    added basic swagger docs

--- app/Modules/Infrastructure/Controllers/BankInfrastructureController.php
+++ app/Modules/Infrastructure/Controllers/BankInfrastructureController.php
@@ -7,11 +7,44 @@ use App\Modules\Bank\Bank;
 use App\Modules\Bank\BankEntity;
 use App\Modules\Bank\Resources\BankResourse;
 use App\Modules\Infrastructure\Entities\BankOfficeEntity;
+use App\Modules\OpenApi\OpenApiDefaults;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class BankInfrastructureController extends Controller
 {
+    use OpenApiDefaults;
+
+    /**
+     * @OA\Get(
+     *     path="/api/bank/{bank}/offices",
+     *     operationId="getAllBankOffices",
+     *     tags={"Bank Infrastructure"},
+     *     summary="Получить список всех офисов банка",
+     *     description="Возвращает список всех офисов банка",
+     *     @OA\Parameter(
+     *          name="bank",
+     *          in="path",
+     *          required=true,
+     *          description="id банка",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation"
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Bad request"
+     *     ),
+     *     @OA\Response(
+     *          response=404,
+     *          description="Не найден банк"
+     *      )
+     * )
+     */
     public function getOfficesForBank(Request $request, Bank $bank): JsonResponse
     {
         $bankEntity = new BankEntity($bank);
@@ -20,6 +53,36 @@ class BankInfrastructureController extends Controller
         return response()->json(BankResourse::collection($offices)->resolve());
     }
 
+    /**
+     * @OA\Get(
+     *     path="/api/bank/{bank}/can-credit/offices",
+     *     operationId="getAllBankCreditOffices",
+     *     tags={"Bank Infrastructure"},
+     *     summary="Получить список всех офисов банка ( в которых можно взять кредит )",
+     *     description="Возвращает список всех офисов банка в которых можно взять кредит",
+     *     @OA\Parameter(
+     *          name="bank",
+     *          in="path",
+     *          required=true,
+     *          description="id банка",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation"
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Bad request"
+     *     ),
+     *     @OA\Response(
+     *          response=404,
+     *          description="Не найден банк"
+     *      )
+     * )
+     */
     public function getOfficesCanCreditForBank(Request $request, Bank $bank): JsonResponse
     {
         $bankEntity = new BankEntity($bank);
